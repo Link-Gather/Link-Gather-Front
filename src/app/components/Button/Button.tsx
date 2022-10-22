50659c20eb95b7e54df7b6c5cb394963e837e0f9
Vin <woqls4@gmail.com>
2022-10-23 00:34:14 +0900

    refactor: 크로마틱 귀찮다..

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -2,7 +2,14 @@ function Button(props: {
   children: React.ReactNode;
   onClick: (e: React.SyntheticEvent<HTMLButtonElement>) => void;
 }) {
-  return <button onClick={props.onClick}>{props.children}</button>;
+  return (
+    <button
+      style={{ backgroundColor: "#000", color: "#FFF" }}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export { Button };

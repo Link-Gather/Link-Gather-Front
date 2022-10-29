174aa5e7a6538183389957aebf1e1a46d2160bc6
Vin <woqls4@gmail.com>
2022-10-29 16:13:52 +0900

    feat: configure storybook config support scss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const webpackConfigResolve = require('../webpack.config.resolve');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -8,5 +10,23 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    });
+
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        extensions: [...config.resolve.extensions, '.png'],
+        alias: {
+          ...config.resolve.alias,
+          ...webpackConfigResolve.alias,
+        },
+      },
+    };
   },
 };

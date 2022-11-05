dfc503ebd73b9ccd74855caa6d2122d87c1060ea
YooJinRa <rarastudy@naver.com>
2022-11-06 05:55:31 +0900

    feat: change storybook settings

--- src/app/components/Dialog/Dialog.stories.tsx
+++ src/app/components/Dialog/Dialog.stories.tsx
@@ -1,12 +1,12 @@
 import { ComponentProps } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import { Dialog } from './Dialog';
+import { DialogButton } from './DialogButton';
 
-type ArgTypes = ComponentProps<typeof Dialog>;
+type ArgTypes = ComponentProps<typeof DialogButton>;
 
 export default {
   title: 'components/Dialog',
-  component: Dialog,
+  component: DialogButton,
   args: {},
 } as Meta<ArgTypes>;
 

ac1fd3eeeed8c7ccba4776e0b58705ec7c64f3f7
Vin <woqls4@gmail.com>
2022-11-14 14:34:54 +0900

    fix: ThemeProvider에서 scss 제거

---
+++ src/app/libs/theme/index.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, ReactNode, useContext, useMemo, useState } from 'react';
+import { Theme, ThemeProvider as EmotionThemeProvider, useTheme as useEmotionTheme } from '@emotion/react';
+import * as palettes from './palettes';
+
+type Palette = keyof typeof palettes;
+
+const PaletteContext = createContext<{
+  setPalette: (theme: Palette) => void;
+}>({
+  setPalette: () => {},
+});
+
+const spacing = (...args: number[]) => args.map((space) => `${space * 4}px`).join(' ');
+
+function ThemeProvider(props: { children: ReactNode }) {
+  // prop destruction
+  const { children } = props;
+
+  // lib hooks
+  // state, ref, querystring hooks
+  const [palette, setPalette] = useState<Palette>('default');
+
+  // form hooks
+  // query hooks
+  // calculated values
+  const paletteSetter = useMemo(() => ({ setPalette }), []);
+  const theme = useMemo(() => {
+    return {
+      palette: palettes[palette],
+      spacing,
+    };
+  }, [palette]);
+
+  // effects
+  // handlers
+
+  return (
+    <PaletteContext.Provider value={paletteSetter}>
+      <EmotionThemeProvider theme={theme}>{children}</EmotionThemeProvider>
+    </PaletteContext.Provider>
+  );
+}
+
+export const useTheme = (): [Theme, (palette: Palette) => void] => {
+  const { setPalette } = useContext(PaletteContext);
+  return [useEmotionTheme(), setPalette];
+};
+
+export { ThemeProvider };
---
+++ src/app/libs/theme/palettes/default.ts
@@ -0,0 +1,22 @@
+const palette = {
+  primary: {
+    light: '#adc2ff',
+    main: '#6887e3',
+    dark: '#4a62a8',
+  },
+  secondary: {
+    light: '#dddddd',
+    main: '#aaaaaa',
+    dark: '#888888',
+  },
+  error: {
+    light: 'rgba(211, 47, 47, 0.6)',
+    main: 'rgba(211, 47, 47, 0.9)',
+    dark: 'rgba(211, 47, 47, 1)',
+  },
+  paper: '#ffffff',
+  text: '#000000',
+  contrastText: '#FFFFFF',
+};
+
+export default palette;

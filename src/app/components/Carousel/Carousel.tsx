1c5d96eb8f2054e8b1012ae0f70c52b005962427
LuisKlopp <fbgus333@gmail.com>
2022-12-01 14:33:05 +0900

    fix: dot,자동슬라이드 삭제 및 코드수정

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState, useMemo } from 'react';
 import styled from '@emotion/styled';
 
 const ArrowButton = styled('button')({
@@ -14,35 +14,34 @@ const ArrowButton = styled('button')({
   },
 });
 
-export function Carousel(props: { images: { id: number; src: string; alt?: string }[]; className: string }) {
-  const imageBox = useRef<HTMLDivElement>(null);
+export function Carousel(props: { images: { id: number; src: string; alt?: string }[]; className?: string }) {
+  // prop destruction
+  const { images, className } = props;
+
+  //lib hooks
+  // state hooks, ref hooks
   const [num, setNum] = useState<number>(1);
   const [carouselTransition, setCarouselTransition] = useState('');
-  const { images, className } = props;
-  const cloneImages = [images[images.length - 1], ...images, images[0]];
+  const imageBox = useRef<HTMLDivElement>(null);
+
+  //form hooks
+  //query hooks
+  //calculated values
+  const cloneImages = useMemo(() => {
+    return [images[images.length - 1], ...images, images[0]];
+  }, [images]);
+
   const lastImage = cloneImages.length - 1;
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setNum((num) => num + 1);
-      setCarouselTransition('transform 500ms ease-in-out');
-    }, 2500);
-    return () => {
-      clearInterval(timer);
-    };
-  }, [cloneImages.length, num]);
-
+  //effects
   useEffect(() => {
     if (num === lastImage) handleOriginSlide(1);
     else if (num === 0) handleOriginSlide(lastImage - 1);
   }, [cloneImages.length, lastImage, num]);
 
-  function handleSlideNext() {
-    if (num < lastImage) setNum((num) => num + 1);
-    setCarouselTransition('transform 500ms ease-in-out');
-  }
-  function handleSlidePrev() {
-    if (num > 0) setNum((num) => num - 1);
+  //handlers
+  function handleSlide(direction: string) {
+    direction === 'prev' ? setNum((num) => num - 1) : setNum((num) => num + 1);
     setCarouselTransition('transform 500ms ease-in-out');
   }
   function handleOriginSlide(index: number): void {
@@ -90,47 +89,6 @@ export function Carousel(props: { images: { id: number; src: string; alt?: strin
       </div>
       <div
         css={{
-          position: 'absolute',
-          width: '150px',
-          marginLeft: '-75px',
-          height: '25px',
-          bottom: '5px',
-          left: '50%',
-          display: 'flex',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-        }}
-      >
-        {images.map((dot) => {
-          return (
-            <div
-              key={dot.src}
-              css={[
-                {
-                  backgroundColor: 'rgb(255, 255, 255)',
-                  opacity: '0.9',
-                  height: '20px',
-                  width: '20px',
-                  borderRadius: '50%',
-                  cursor: 'pointer',
-                  '&:hover': {
-                    backgroundColor: 'rgb(182, 182, 182)',
-                  },
-                },
-                dot.id + 1 !== num && {
-                  backgroundColor: 'rgb(182, 182, 182)',
-                  border: '2px solid white',
-                },
-              ]}
-              onClick={() => {
-                setNum(dot.id + 1);
-              }}
-            />
-          );
-        })}
-      </div>
-      <div
-        css={{
           height: '100px',
           position: 'absolute',
           marginTop: '-50px',
@@ -141,8 +99,16 @@ export function Carousel(props: { images: { id: number; src: string; alt?: strin
           width: '100%',
         }}
       >
-        <ArrowButton onClick={handleSlidePrev} />
-        <ArrowButton onClick={handleSlideNext} />
+        <ArrowButton
+          onClick={() => {
+            handleSlide('prev');
+          }}
+        />
+        <ArrowButton
+          onClick={() => {
+            handleSlide('next');
+          }}
+        />
       </div>
     </div>
   );

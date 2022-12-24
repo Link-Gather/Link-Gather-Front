b81fe81a9812b9d43ececc8ef473e13c42346aac
YooJinRa <rarastudy@naver.com>
2022-12-24 17:42:05 +0900

    fix: customhook 주석 삭제

--- src/app/hooks/useDialog/index.ts
+++ src/app/hooks/useDialog/index.ts
@@ -1,17 +1,8 @@
 import { useState } from 'react';
 
 export const useDialog = () => {
-  // prop destruction
-  // lib hooks
-  // state, ref hooks
   const [isOpenDialog, setIsOpenDialog] = useState<boolean>(false);
 
-  // form hook
-  // query hooks
-  // calculated values
-  // effects
-
-  // handlers
   const openDialog = () => {
     setIsOpenDialog(true);
   };

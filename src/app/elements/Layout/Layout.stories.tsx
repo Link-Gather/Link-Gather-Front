11350e0c3526c525661784838beb332fcf265f6d
YooJinRa <rarastudy@naver.com>
2023-02-05 16:17:29 +0900

    fix: test storybook error

--- src/app/elements/Layout/Layout.stories.tsx
+++ src/app/elements/Layout/Layout.stories.tsx
@@ -7,8 +7,18 @@ type ArgTypes = ComponentProps<typeof Layout>;
 export default {
   title: 'elements/Layout',
   component: Layout,
-  args: {},
-  argTypes: {},
+  args: {
+    children: (
+      <>
+        <div css={{ border: '1px solid #aaaaaa' }}>First Child</div>
+        <div css={{ border: '1px solid #aaaaaa' }}>Second Child</div>
+        <div css={{ border: '1px solid #aaaaaa' }}>Third Child</div>
+      </>
+    ),
+  },
+  argTypes: {
+    componentStyle: { type: 'string' },
+  },
 } as Meta<ArgTypes>;
 
 export const Default: StoryObj<ArgTypes> = {};

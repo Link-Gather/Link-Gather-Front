f1df24284d4ce528da65ba8ab1bef15c8aad65de
Windy <ydg0330@naver.com>
2023-02-06 22:06:04 +0900

    fix: kakao client id

--- src/app/configs/index.ts
+++ src/app/configs/index.ts
@@ -1,7 +1,7 @@
 export const API_ENDPOINT = process.env.API_ENDPOINT;
 
 // TODO: 임시로 발급받은 것. 서비스용으로 카카오앱 생성하면 대채할 것!!
-export const KAKAO_CLIENT_ID = '305bb38aacda45d3d802e563c3cd167f';
+export const KAKAO_CLIENT_ID = 'e12018dad709f364d07c57d260efab27';
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 

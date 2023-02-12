e0d2452091c0facb815767e686cad48e738d829d
YooJinRa <rarastudy@naver.com>
2023-02-12 22:42:13 +0900

    feat: login screen 배경 반응형 적용

--- src/app/screens/login/LoginScreen.tsx
+++ src/app/screens/login/LoginScreen.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FlexBox, UnderlineTitle } from '@elements';
 import { LoginForm, LoginBottomInfo, OauthContainer, ShadowBox } from '@components';
-import BackgroundLoginOne from '@assets/images/backgrounds/background-login-1.svg';
+import BackgroundPlanetPrimary from '@assets/images/backgrounds/background-planet-primary.svg';
+import BackgroundAstronautPrimary from '@assets/images/backgrounds/background-astronaut-primary.svg';
 import IconArrowLeft from '@assets/images/icons/icon-arrow-left.svg';
 import { type Theme, mq } from '@libs/theme';
 
@@ -37,21 +38,57 @@ function LoginScreen() {
           {
             position: 'fixed',
             left: '0',
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center',
             width: '58vw',
             height: '100%',
-            backgroundImage: `url(${BackgroundLoginOne})`,
+            backgroundImage: `url(${BackgroundPlanetPrimary})`,
             backgroundRepeat: 'no-repeat',
             backgroundSize: '100% auto',
-            backgroundPosition: 'left bottom',
+            backgroundPosition: 'left -5vw bottom -40vh',
             zIndex: 0,
 
+            [mq[0]]: {
+              backgroundPosition: 'left 0 bottom -30vh',
+            },
+            [mq[1]]: {
+              backgroundPosition: 'left 0 bottom -20vh',
+            },
             [mq[2]]: {
               width: '80vw',
               marginLeft: '10vw',
+              backgroundPosition: 'left 0 bottom -15vh',
             },
           },
         ]}
-      />
+      >
+        <div
+          css={() => [
+            {
+              position: 'absolute',
+              width: '34%',
+              height: '100%',
+              backgroundImage: `url(${BackgroundAstronautPrimary})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: '100% auto',
+              backgroundPosition: 'left 0 bottom 50vh',
+              marginLeft: '-5vw',
+
+              [mq[0]]: {
+                backgroundPosition: 'left 0 bottom 30vh',
+                marginLeft: '0',
+              },
+              [mq[1]]: {
+                backgroundPosition: 'left 0 bottom 24vh',
+              },
+              [mq[2]]: {
+                backgroundPosition: 'left 0 bottom 22vh',
+              },
+            },
+          ]}
+        />
+      </div>
       <ShadowBox
         padding='40px'
         css={{

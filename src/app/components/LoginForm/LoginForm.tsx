5dbdfce7e21e628fb724bb8f967eddc1c76f4fcd
YooJinRa <rarastudy@naver.com>
2023-03-08 03:07:09 +0900

    feat: login input 관련하여 코드 정리

--- src/app/components/LoginForm/LoginForm.tsx
+++ src/app/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Input, FlexBox } from '@elements';
+import IconCheckGreen from '@assets/images/icons/icon-check-green.svg';
+import IconPasswordShow from '@assets/images/icons/icon-password-show.svg';
+import IconPasswordHide from '@assets/images/icons/icon-password-hide.svg';
 import palette from '@libs/theme/palettes';
 
 function LoginForm() {
   // prop destruction
   // lib hooks
   // state, ref hooks
-
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
+  const [isEmail, setIsEmail] = useState<boolean>(false);
+  const [isValue, setIsValue] = useState<boolean>(false);
   // form hooks
   // query hooks
   // calculated values
   // effects
   // handlers
+  const handlePasswordShow = () => {
+    setIsShowPassword(!isShowPassword);
+  };
+
+  // TODO : 함수 따로 빼서 관리, 타입 어떻게 관리할지 논의 필요
+  const checkValidation = (type: 'password' | 'email', value: string) => {
+    const pattern: Record<'password' | 'email', RegExp> = {
+      email: /^[^s@]+@[^s@]+.[^s@]+$/,
+      password: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$/,
+    };
+
+    return pattern[type].test(value);
+  };
+  const checkEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    setIsEmail(checkValidation('email', event.currentTarget.value));
+  };
+
+  const checkPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsValue(event.currentTarget.value.length > 0);
+  };
 
   return (
     <FlexBox direction='column' spacing={4}>
-      <Input type='email' width='100%' height={50} placeholder='email' />
-      <Input type='password' minLength={8} maxLength={16} width='100%' height={50} placeholder='password' />
+      <Input type='email' width='100%' height={50} placeholder='email' onChange={checkEmail}>
+        {isEmail && <img src={IconCheckGreen} alt='checked email' />}
+      </Input>
+      <Input
+        type={isShowPassword ? 'text' : 'password'}
+        minLength={8}
+        maxLength={16}
+        width='100%'
+        height={50}
+        placeholder='password'
+        onClick={handlePasswordShow}
+      >
+        <img src={isShowPassword ? IconPasswordShow : IconPasswordHide} alt='password' />
+      </Input>
       <Button
         width='100%'
         height={48}
--- src/app/elements/Input/Input.tsx
+++ src/app/elements/Input/Input.tsx
@@ -1,7 +1,4 @@
-import { CSSProperties, useId, useState, useRef } from 'react';
-import IconCheckGreen from '@assets/images/icons/icon-check-green.svg';
-import IconPasswordShow from '@assets/images/icons/icon-password-show.svg';
-import IconPasswordHide from '@assets/images/icons/icon-password-hide.svg';
+import { CSSProperties, useId, useRef } from 'react';
 import type { Theme } from '@libs/theme';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -15,6 +12,8 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   marginTop?: CSSProperties['marginTop'];
   marginLeft?: CSSProperties['marginLeft'];
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick?: () => void;
+  children?: React.ReactNode;
 }
 
 function Input(props: Props) {
@@ -30,7 +29,9 @@ function Input(props: Props) {
     color,
     backgroundColor,
     onChange,
+    onClick,
     className,
+    children,
     ...rest
   } = props;
 
@@ -39,33 +40,13 @@ function Input(props: Props) {
 
   // state, ref hooks
   const inputRef = useRef<HTMLInputElement>(null);
-  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
-  const [isEmail, setIsEmail] = useState<boolean>(false);
-  const [isValue, setIsValue] = useState<boolean>(false);
+
   // form hook
   // query hooks
   // calculated values
   // effects
 
   // handlers
-  const handlePasswordShow = () => {
-    setIsShowPassword(!isShowPassword);
-    if (inputRef.current) {
-      inputRef.current.type = isShowPassword ? 'password' : 'text';
-    }
-  };
-
-  const validateEmail = (email: string) => {
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return pattern.test(email);
-  };
-  const checkEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsEmail(validateEmail(event.currentTarget.value));
-  };
-
-  const checkPassword = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setIsValue(event.currentTarget.value.length > 0);
-  };
 
   return (
     <label
@@ -97,9 +78,6 @@ function Input(props: Props) {
               },
               '&:focus': {
                 border: `2px solid ${theme.palette.primary.main}`,
-                '& + button': {
-                  opacity: '1',
-                },
               },
               '&[inputType="error"]': {
                 border: `2px solid ${theme.palette.secondary.red}`,
@@ -108,7 +86,7 @@ function Input(props: Props) {
           ];
         }}
         className={className}
-        onChange={type === 'email' ? checkEmail : type === 'password' ? checkPassword : onChange}
+        onChange={onChange}
         ref={inputRef}
         {...rest}
       />
@@ -124,14 +102,10 @@ function Input(props: Props) {
           border: 'none',
           background: 'none',
           cursor: 'pointer',
-          opacity: '0',
         }}
-        onClick={type === 'password' ? handlePasswordShow : () => {}}
+        onClick={onClick}
       >
-        {type === 'email' && isEmail && <img src={IconCheckGreen} alt='check' />}
-        {type === 'password' && isValue && (
-          <img src={isShowPassword ? IconPasswordShow : IconPasswordHide} alt='password' />
-        )}
+        {children}
       </button>
     </label>
   );

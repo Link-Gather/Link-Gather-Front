eea58404a80a2824dfc0ec139eb3f575b9d0d36d
YooJinRa <rarastudy@naver.com>
2023-03-12 14:03:22 +0900

    feat: KakaoLoginButton storybook 추가

---
+++ src/app/elements/KakaoLoginButton/KakaoLoginButton.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { KakaoLoginButton } from './KakaoLoginButton';
+
+type ArgTypes = ComponentProps<typeof KakaoLoginButton>;
+
+export default {
+  title: 'elements/KakaoLoginButton',
+  component: KakaoLoginButton,
+  args: {},
+} as Meta<ArgTypes>;
+
+export const Default: StoryObj<ArgTypes> = {};

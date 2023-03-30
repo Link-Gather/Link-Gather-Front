0c7a4cac4447e4d83223422eb8cc204f0cdee1ca
LuisKlopp <fbgus333@gmail.com>
2023-03-30 21:30:24 +0900

    refactoring: 스토리북 해결중

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,11 +2,7 @@ const webpackConfigResolve = require('../webpack.config.resolve');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',

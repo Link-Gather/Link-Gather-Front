62ea8c253e0c339bf94ba8ba5636339ee6964db9
YooJinRa <rarastudy@naver.com>
2023-04-21 21:22:22 +0900

    feat: header ui 작업 진행중

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import { FlexBox } from '@elements';
+import palette from '@libs/theme/palettes';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <FlexBox
+      width='100%'
+      height='70px'
+      alignItems='center'
+      justifyContent='center'
+      css={{
+        borderBottom: `2px solid ${palette.black.main}`,
+      }}
+    >
+      <FlexBox
+        width='100%'
+        height='100%'
+        alignItems='center'
+        justifyContent='space-between'
+        css={{
+          maxWidth: '1352px',
+          backgroundColor: palette.primary.main,
+        }}
+      >
+        <h1>
+          <Link to='/'>Logo</Link>
+        </h1>
+      </FlexBox>
+    </FlexBox>
   );
 }
 

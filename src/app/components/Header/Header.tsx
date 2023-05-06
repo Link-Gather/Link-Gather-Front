2ca9753fbaab814ba2eb060dcf06196bef044e25
YooJinRa <rarastudy@naver.com>
2023-05-06 18:01:17 +0900

    feat: 비로그인시 header publishing & path import하는 방식으로 변경

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,9 +2,12 @@ import { Link, NavLink } from 'react-router-dom';
 import { Button } from '@elements';
 import LogoHeader from '@assets/images/logos/logo-header.svg'; // TODO: 이미지 경로 SW-90에서 수정 될 예정
 import ProfileDefault from '@assets/images/profiles/profile-default.svg'; // TODO: 이미지 경로 SW-90에서 수정 될 예정
+import ProfileNoUser from '@assets/images/profiles/profile-no-user.svg'; // TODO: 이미지 경로 SW-90에서 수정 될 예정
 import styled from '@emotion/styled';
 import { Stack } from '@mui/material';
+import { isToken } from '@libs/util';
 import type { Theme } from '@libs/theme';
+import { PATH_LOGIN, PATH_PROJECTS, PATH_PROJECTS_ADD } from '@routes';
 
 function Header() {
   return (
@@ -44,30 +47,32 @@ function Header() {
           </h1>
         </Link>
         <Stack height='100%' flexDirection='row' alignItems='center' gap='16px'>
-          <Nav to='/projects'>프로젝트 찾기</Nav>
+          <Nav to={PATH_PROJECTS}>프로젝트 찾기</Nav>
           <Nav to='/find-co-workers'>동료 찾기</Nav>
           <Nav to='/manage-projects'>프로젝트 관리</Nav>
         </Stack>
         <Stack height='100%' flexDirection='row' alignItems='center'>
-          <Button
-            css={(theme: Theme) => ({
-              width: '212px',
-              height: '100%',
-              fontFamily: '"Noto Sans KR", sans-serif',
-              fontSize: '16px',
-              fontWeight: '700',
-              color: theme.palette.paper,
-              borderLeft: `2px solid ${theme.palette.black.main}`,
-              borderRight: `2px solid ${theme.palette.black.main}`,
-              backgroundColor: theme.palette.primary.main,
-              '&:hover': {
-                textDecoration: 'underline',
-                backgroundColor: theme.palette.black.main,
-              },
-            })}
-          >
-            프로젝트생성
-          </Button>
+          <Link to={isToken() ? PATH_PROJECTS_ADD : PATH_LOGIN} css={{ height: '100%' }}>
+            <Button
+              css={(theme: Theme) => ({
+                width: '212px',
+                height: '100%',
+                fontFamily: '"Noto Sans KR", sans-serif',
+                fontSize: '16px',
+                fontWeight: '700',
+                color: theme.palette.paper,
+                borderLeft: `2px solid ${theme.palette.black.main}`,
+                borderRight: `2px solid ${theme.palette.black.main}`,
+                backgroundColor: theme.palette.primary.main,
+                '&:hover': {
+                  textDecoration: 'underline',
+                  backgroundColor: theme.palette.black.main,
+                },
+              })}
+            >
+              {isToken() ? '프로젝트생성' : '로그인'}
+            </Button>
+          </Link>
           <Stack
             width='112px'
             height='100%'
@@ -83,19 +88,28 @@ function Header() {
                 width: '50px',
                 height: '50px',
                 borderRadius: '50%',
-                border: `2px solid ${theme.palette.black.main}`,
-                backgroundColor: theme.palette.primary.main,
+                border: `2px solid ${isToken() ? theme.palette.black.main : theme.palette.secondary.n300}`,
+                backgroundColor: isToken() ? theme.palette.primary.main : theme.palette.secondary.n30,
                 overflow: 'hidden',
               })}
             >
-              <ProfileDefault
-                css={{
-                  width: '200%',
-                  height: '200%',
-                  objectFit: 'contain',
-                  transform: 'translate(25%, 20%)',
-                }}
-              />
+              {isToken() ? (
+                <ProfileDefault
+                  css={{
+                    width: '200%',
+                    height: '200%',
+                    objectFit: 'contain',
+                    transform: 'translate(25%, 20%)',
+                  }}
+                />
+              ) : (
+                <ProfileNoUser
+                  css={{
+                    width: '11px',
+                    height: '20px',
+                  }}
+                />
+              )}
             </Button>
           </Stack>
         </Stack>

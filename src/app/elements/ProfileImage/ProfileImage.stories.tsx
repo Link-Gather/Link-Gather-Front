aceb4fb30858855505bbd4c3ac6b85f0cf8fe6a9
changhwan hwang <ch.hwang@ecubelabs.com>
2023-05-20 18:32:21 +0900

    story: add all

--- src/app/elements/ProfileImage/ProfileImage.stories.tsx
+++ src/app/elements/ProfileImage/ProfileImage.stories.tsx
@@ -2,12 +2,16 @@ import type { ComponentProps } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { ProfileImage } from './ProfileImage';
 import { S3_IMAGE_BUCKET } from '../../configs';
+import { Stack } from '@mui/material';
 
 type ArgTypes = ComponentProps<typeof ProfileImage>;
 
 export default {
   title: 'elements/ProfileImage',
   component: ProfileImage,
+  args: {
+    src: `${S3_IMAGE_BUCKET}/lizard.svg`,
+  },
 } as Meta<ArgTypes>;
 
 export const Lizard: StoryObj<ArgTypes> = {
@@ -50,4 +54,19 @@ export const Peanut: StoryObj<ArgTypes> = {
   args: {
     src: `${S3_IMAGE_BUCKET}/peanut.svg`,
   },
+};
+
+export const All: StoryObj<ArgTypes> = {
+  render: () => (
+    <Stack direction='row' spacing='16px'>
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/lizard.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/grey.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/pink.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/flower.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/pigtail.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/orange.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/yellow.svg`} />
+      <ProfileImage src={`${S3_IMAGE_BUCKET}/peanut.svg`} />
+    </Stack>
+  ),
 };

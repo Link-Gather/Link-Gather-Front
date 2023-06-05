33725f1e8a653f554aa9622b6a9e86db45a643f4
changhwan hwang <ch.hwang@ecubelabs.com>
2023-06-05 18:53:55 +0900

    fix: minor

--- src/app/screens/sign-up/success/SignUpSuccessScreen.tsx
+++ src/app/screens/sign-up/success/SignUpSuccessScreen.tsx
@@ -16,36 +16,37 @@ function SignUpSuccessScreen(props: {}) {
   // effects
   // handlers
   return (
-    <Stack
-      direction='row'
-      spacing='94px'
-      css={{ alignItems: 'center', justifyContent: 'center', height: '100%', position: 'relative' }}
-    >
+    <Stack css={{ alignItems: 'center', justifyContent: 'center', height: '100%', position: 'relative' }}>
       <YellowStar
         css={{ width: '68px', position: 'absolute', transform: 'rotate(24.73deg)', top: '25%', left: '30%' }}
       />
       <YellowStar
         css={{ width: '48px', position: 'absolute', transform: 'rotate(-59.47deg)', top: '34%', left: '50%' }}
       />
-      <Stack direction='column' spacing='40px'>
-        <Typography variant='h1' css={{ fontSize: '64px', fontWeight: 800 }}>
-          Welcome!
-        </Typography>
-        <Typography variant='body1' css={(theme: Theme) => ({ fontWeight: 500, color: theme.palette.secondary.n300 })}>
-          링크게더 가입이 완료되었습니다!
-          <br />
-          오늘은 어떤 흥미로운 프로젝트가 있는지 같이 확인해볼까요?
-        </Typography>
-        <Stack direction='row' spacing='24px'>
-          <Button css={{ flex: 1 }} onClick={() => navigate(PATH_HOME)}>
-            홈으로
-          </Button>
-          <Button variant='filled' css={{ flex: 1.4 }} onClick={() => navigate(PATH_LOGIN)}>
-            로그인
-          </Button>
+      <Stack direction='row' spacing='94px' css={{ alignItems: 'end' }}>
+        <Stack direction='column' spacing='40px'>
+          <Typography variant='h1' css={{ fontSize: '64px', fontWeight: 800 }}>
+            Welcome!
+          </Typography>
+          <Typography
+            variant='body1'
+            css={(theme: Theme) => ({ fontWeight: 500, color: theme.palette.secondary.n300 })}
+          >
+            링크게더 가입이 완료되었습니다!
+            <br />
+            오늘은 어떤 흥미로운 프로젝트가 있는지 같이 확인해볼까요?
+          </Typography>
+          <Stack direction='row' spacing='24px'>
+            <Button css={{ flex: 1 }} onClick={() => navigate(PATH_HOME)}>
+              홈으로
+            </Button>
+            <Button variant='filled' css={{ flex: 1.4 }} onClick={() => navigate(PATH_LOGIN)}>
+              로그인
+            </Button>
+          </Stack>
         </Stack>
+        <Astronaut css={{ width: '144px' }} />
       </Stack>
-      <Astronaut css={{ maxWidth: '144px' }} />
     </Stack>
   );
 }

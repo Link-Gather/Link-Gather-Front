f1bff600793a77d7e148532803dcf209dc396ae2
jeangho293 <jeangho293@naver.com>
2023-06-18 19:59:01 +0900

    fix: 비밀번호 재확인 input error message 수정

--- src/app/libs/schema/index.ts
+++ src/app/libs/schema/index.ts
@@ -14,4 +14,4 @@ export const SCHEMA_NICKNAME = yup
   .matches(/^[a-zA-Z0-9가-힣]+$/, '8자이내, 한글, 영문 숫자 혼용 가능')
   .min(1)
   .max(8);
-export const SCHEMA_CONFIRM_PASSWORD = yup.string().oneOf([yup.ref('password')], '비밀번호가 일치하지 않습니다.');
+export const SCHEMA_CONFIRM_PASSWORD = yup.string().oneOf([yup.ref('password')], '비밀번호가 일치하지 않습니다 :(');
